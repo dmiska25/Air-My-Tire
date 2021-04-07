1d89c069b1334bbaa1d134c0d2c83b77b9d4a01c
unknown <ben.ockert@gmail.com>
2021-04-06 22:51:13 -0500

    Addition of componets instead of modules

---
+++ src/app/highlighted-view/highlighted-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-highlighted-view',
+  templateUrl: './highlighted-view.component.html',
+  styleUrls: ['./highlighted-view.component.css']
+})
+export class HighlightedViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/station-card/station-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-station-card',
+  templateUrl: './station-card.component.html',
+  styleUrls: ['./station-card.component.css']
+})
+export class StationCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
